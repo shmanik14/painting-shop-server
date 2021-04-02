b39994c213afbdb568e79cb5f07137e89e0820fe
sahadat <shmanik14@yahoo.com>
2021-04-02 16:11:26 +0600

    add

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const objectId = require('mongodb').ObjectId;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cxkd4.mongodb.net/volunteer?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -31,11 +32,33 @@ client.connect(err => {
           res.send(documents);
         })
       })
+    
+    app.get('/checkout/:id', (req, res) => {
+      paintCollection.find({_id: objectId(req.params.id)})
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      })
+    })
+
+    app.get("/manageProduct", (req,res) => {
+      paintCollection.find({email: req.query.email})
+      .toArray((err, documents) => {
+      res.send(documents);
+      })
+    })
+
+    app.delete("/delete/:id", (req, res) => {
+      paintCollection.findOneAndDelete({_id: objectId(req.params.id)})
+      .then(result => {
+        console.log(result)
+        res.send(result.deletedCount > 0);
+      })
+    })
 
 })
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello Painting Precision')
   })
   
 app.listen(port)
